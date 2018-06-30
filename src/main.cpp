0e4d839ea92583d7d61aae9f0a379ffe7dd78dfa
Daniel Kucera <daniel.kucera@gmail.com>
2018-06-30 12:55:00 +0200

    remotely driven display

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "LedMatrix.h"
-#include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
 const char *ssid     = "coloseum";
 const char *password = "Kucerovisu1";
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 7200, 60000);
+WiFiUDP Udp;
+unsigned int localUdpPort = 1234;
 
 #define NUMBER_OF_DEVICES 4
 #define CS_PIN 4
@@ -32,35 +31,33 @@ void setup() {
     delay ( 500 );
   }
 
-  timeClient.begin();
+  int lastOctet = WiFi.localIP()[3];
+
+  ledMatrix.clear();
+  ledMatrix.setTextAlignment(TEXT_ALIGN_LEFT);
+  ledMatrix.setText(String(lastOctet));
+  ledMatrix.drawText();
+  ledMatrix.commit();
+
+  Udp.begin(localUdpPort);
 }
 
-char text[10];
+char incomingPacket[255];
 
 void loop() {
-  timeClient.update();
-  ledMatrix.clear();
 
-  sprintf(text, "%2d%2d" , timeClient.getHours(), timeClient.getMinutes());
-
-  ledMatrix.setTextAlignment(TEXT_ALIGN_LEFT);
-  ledMatrix.setText(text);
-  ledMatrix.drawText();
-
-/*
-  ledMatrix.setTextAlignment(TEXT_ALIGN_LEFT);
-  ledMatrix.setText(String(timeClient.getHours()));
-  ledMatrix.drawText();
-
-  ledMatrix.setTextAlignment(TEXT_ALIGN_RIGHT);
-  ledMatrix.setText(":");
-  ledMatrix.drawText();
-
-  ledMatrix.setTextAlignment(TEXT_ALIGN_RIGHT);
-  ledMatrix.setText(String(timeClient.getMinutes()));
-  ledMatrix.drawText();
-*/
-
-  ledMatrix.commit(); // commit transfers the byte buffer to the displays
-  delay(1000);
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      ledMatrix.clear();
+      for (int i=0; (i<8*NUMBER_OF_DEVICES) && (i<packetSize); i++){
+        ledMatrix.setColumn(i, incomingPacket[i]);
+        //ledMatrix.setColumn(i, 0xAA);
+      }
+      ledMatrix.commit(); // commit transfers the byte buffer to the displays
+    }
+  }
 }
